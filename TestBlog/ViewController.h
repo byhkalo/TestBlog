341055f185233b78bf1ffc3fa52d12ef7542ca8b
Bykhkalo.Konstantyn <byhkalo.konstantyh@gmail.com>
2016-03-30 22:31:44 +0300

    Initial Commit

---
+++ TestBlog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBlog
+//
+//  Created by Konstantyn Bykhkalo on 30.03.16.
+//  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestBlogTests/TestBlogTests.m
@@ -0,0 +1,39 @@
+//
+//  TestBlogTests.m
+//  TestBlogTests
+//
+//  Created by Konstantyn Bykhkalo on 30.03.16.
+//  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestBlogTests : XCTestCase
+
+@end
+
+@implementation TestBlogTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
