c94d044d4b5824ac72be488be7612c8e8454a0b8
Bykhkalo.Konstantyn <byhkalo.konstantyh@gmail.com>
2016-03-30 23:13:11 +0300

    Create KBBlogView
    Connect KBBlogViewController and KVBlogView with storyboard

---
+++ TestBlog/Controllers/KBBlogController/KBBlogView.h
@@ -0,0 +1,13 @@
+//
+//  KBBlogView.h
+//  TestBlog
+//
+//  Created by Konstantyn Bykhkalo on 30.03.16.
+//  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBBlogView : UIView
+
+@end
---
+++ TestBlog/Controllers/KBBlogController/KBBlogView.m
@@ -0,0 +1,21 @@
+//
+//  KBBlogView.m
+//  TestBlog
+//
+//  Created by Konstantyn Bykhkalo on 30.03.16.
+//  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import "KBBlogView.h"
+
+@implementation KBBlogView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
