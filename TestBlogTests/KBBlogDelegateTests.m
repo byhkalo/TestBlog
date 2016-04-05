76da37ac5683e4b61bd022041b8608577b8fe54d
Bykhkalo.Konstantyn <byhkalo.konstantyh@gmail.com>
2016-04-06 01:32:08 +0300

    add empty tests for KBBlogDelegate

--- TestBlogTests/KBBlogDelegateTests.m
+++ TestBlogTests/KBBlogDelegateTests.m
@@ -6,34 +6,44 @@
 //  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import <Kiwi.h>
 
-@interface KBBlogDelegateTests : XCTestCase
+#import "KBBlogDelegate.h"
 
-@end
+SPEC_BEGIN(KBBlogDelegateSpec)
 
-@implementation KBBlogDelegateTests
+describe(@"KBBlogDelegateSpec", ^{
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+    __block KBBlogDelegate *blogDelegate = nil;
+    
+    context(@"when create new delegate should get posts from networkManager", ^{
+        
+        beforeEach(^{
+            blogDelegate = [[KBBlogDelegate alloc] init];
+        });
+        
+        afterEach(^{
+            blogDelegate = nil;
+        });
+        
+        it(@"should not be nil", ^{
+            [[blogDelegate shouldNot] beNil];
+        });
+        
+        it(@"should get posts from network", ^{
+           
+        });
+    });
+    
+    context(@"when downloaded first pack of post", ^{
+        it(@"should set downloaded posts to postArray property", ^{
+            
+        });
+        
+        it(@"should reload tableView", ^{
+            
+        });
+    });
+});
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SPEC_END
