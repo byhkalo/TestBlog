ccc023db7f60b4aef3ee5d76cc946a14bc4a795a
Bykhkalo.Konstantyn <byhkalo.konstantyh@gmail.com>
2016-04-06 02:17:40 +0300

    add xib and view for KBBlogTableCell

---
+++ TestBlog/Controllers/KBBlogController/KBBlogTableCell/KBBlogTableCell.m
@@ -0,0 +1,13 @@
+//
+//  KBBlogTableCell.m
+//  TestBlog
+//
+//  Created by Konstantyn Bykhkalo on 06.04.16.
+//  Copyright © 2016 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import "KBBlogTableCell.h"
+
+@implementation KBBlogTableCell
+
+@end
